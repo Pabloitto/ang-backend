fa3fcf811644111aa920f0923f7f236675c97804
pabloitto <juanitto_40@hotmail.com>
2018-02-08 11:46:16 -0700

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./server')
 const config = require('./config')()
 const init = async () => {
   await server.initDB(config.connectionString)
-  server.init(8085)
+  server.init(process.env.PORT || 8085)
 }
 
 init()
