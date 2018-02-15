695490e7446d950b7669f7d3e3dea0c620cf4c7d
pabloitto <juanitto_40@hotmail.com>
2018-02-15 14:18:28 -0700

    remove auth

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -3,8 +3,8 @@ const userRouter = express.Router()
 const userController = require('../controllers/user-controller')()
 const { sessionFilter } = require('../middlewares/auth')
 module.exports = () => {
-  userRouter.get('/api/users', sessionFilter, userController.fetchUsers)
-  userRouter.get('/api/users/:userId', sessionFilter, userController.fetchUserById)
+  userRouter.get('/api/users', userController.fetchUsers)
+  userRouter.get('/api/users/:userId', userController.fetchUserById)
   userRouter.post('/api/users', userController.save)
   return userRouter
 }
